7cc2cdb01f6782c20e17bd90f20495549efe1b66
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-02-19 01:02:26 +0000

    dice + tacos controllers and index files updates

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,7 @@
 class TacosController < ApplicationController
 
   def index
+    @fillings = ["Mushrooms", "Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
     # render :template => "tacos/index"
   end
 
